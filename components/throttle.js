8e36915ce15ee48812bc27f1c9b89662bc5c6784
danielJin100 <75294070+danielJin100@users.noreply.github.com>
2024-07-13 10:32:00 -0700

    Add files via upload

---
+++ components/throttle.js
@@ -0,0 +1,13 @@
+var speed = 0
+
+
+
+AFRAME.registerComponent(speed, {
+    tick: (function(){
+        $(document).ready(function(){
+            $("#indicator").css("height", 30)
+            $("#indicator").css("margin-top", 20)
+    })
+
+  })  
+})
